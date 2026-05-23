371166b4f2055cea5f71fff94aaa41d73995af81
nunesgpadv-dot <287282606+nunesgpadv-dot@users.noreply.github.com>
2026-05-23 15:25:07 -0300

    Fix PWA paths for GitHub Pages subpath

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 const CACHE_NAME = 'bussola-v2';
+const BASE = self.registration.scope;
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
